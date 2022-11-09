3fe090871fbe8c124e707882d7359087589979ee
Rajeev TC <rajeev@hivelocity.co.jp>
2022-11-09 13:24:40 +0900

    chore: Initial Android sample Glay project commit

---
+++ app/src/main/java/co/jp/hivelocity/glay/MainActivity.java
@@ -0,0 +1,16 @@
+package co.jp.hivelocity.glay;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import co.jp.hivelocity.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
