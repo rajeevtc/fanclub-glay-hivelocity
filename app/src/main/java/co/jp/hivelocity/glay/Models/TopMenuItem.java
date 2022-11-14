73479869d6f74e3b4ea5551bec0fb23b61eb301e
Rajeev TC <rajivdualsh@gmail.com>
2022-11-14 11:39:52 +0900

    feat: Creates the Top menu item list model data

--- app/src/main/java/co/jp/hivelocity/glay/Models/TopMenuItem.java
+++ app/src/main/java/co/jp/hivelocity/glay/Models/TopMenuItem.java
@@ -1,24 +1,54 @@
 package co.jp.hivelocity.glay.Models;
 
+import co.jp.hivelocity.R;
+
 public enum TopMenuItem {
     Music,
     Movie,
     News,
+    LiveStreaming,
+    Photos,
     ARCamera,
+    Profile,
     ;
 
-    String imageName(TopMenuItem type) {
-        switch (type) {
+    public int imageName() {
+        switch (this) {
             case Movie:
-                return "top_icon_video";
+                return R.drawable.top_icon_video;
             case Music:
-                return "top_icon_music";
+                return R.drawable.top_icon_music;
             case News:
-                return "top_icon_news";
+                return R.drawable.top_icon_news;
             case ARCamera:
-                return "top_icon_arcamera";
-            default:
-                return "top_icon_music";
+                return R.drawable.top_icon_arcamera;
+            case LiveStreaming:
+                return R.drawable.top_icon_streaming;
+            case Photos:
+                return R.drawable.top_icon_photo;
+            case Profile:
+                return R.drawable.top_icon_profile;
         }
+        return R.drawable.top_icon_music;
+    }
+
+    public String title() {
+        switch (this) {
+            case Movie:
+                return "Movie";
+            case Music:
+                return "Music";
+            case News:
+                return "News";
+            case ARCamera:
+                return "AR Camera";
+            case LiveStreaming:
+                return "Live";
+            case Photos:
+                return "Photos";
+            case Profile:
+                return "Profile";
+        }
+        return "Music";
     }
 }
